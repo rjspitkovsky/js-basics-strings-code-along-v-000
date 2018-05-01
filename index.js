91469efac1b9750a8e4e8e8934ec3a17c53ea595
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-01 17:35:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your code in this file!
 const currentUser = 'Richie Rich';
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}`
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
