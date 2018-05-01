24d9879a5298b085123e25931946f3fdbeebeeb1
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-01 17:39:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 const currentUser = 'Richie Rich';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser}!`;
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
